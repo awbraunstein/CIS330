3f72ef31ef055b8e1922dcb9816790bc86bda436
Zachary Wasserman <zach@seas006.wlan.seas.upenn.edu>
2011-02-27 15:59:02 -0500

    added list and beginning of tweet schema to schema.sql

---
+++ schema.sql
@@ -0,0 +1,12 @@
+create table list
+       (lid integer,
+       name varchar(30),
+       privacy integer,
+       primary key(lid))
+
+create table tweet
+       (tid integer,
+       posttime date,
+       body varchar(140),
+       
+     
