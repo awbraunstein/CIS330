25efbae6e5f3bda7e87f6cf8a041d54d115e1b95
Zachary Wasserman <zach@Zach-Wassermans-MacBook-Pro.local>
2011-03-07 19:01:43 -0500

    testing some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
   belongs_to :user
+  
+  validates_presence_of :username, :firstname, :lastname, :email, :privacy
+  validates_format_of :email,
+                      :with =>  /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                      :message => "Invalid email format"
 end
