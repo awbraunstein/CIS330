d6da14f998497d0c17bff480222fff1103312989
Zachary Wasserman <zach@seas537.wireless-pennnet.upenn.edu>
2011-03-17 17:36:32 -0400

    added schema info

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,18 @@
+# == Schema Information
+# Schema version: 20110307224716
+#
+# Table name: tweets
+#
+#  id         :integer         not null, primary key
+#  user_id    :integer
+#  body       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Tweet < ActiveRecord::Base
   belongs_to :user
   
+  attr_accessible :body
   validates :body, :length => {:maximum => 140}
 end
