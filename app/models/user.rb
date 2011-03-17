075a6473c5b137b5085f7a4589de3641b9a922c3
Zachary Wasserman <zach@seas537.wireless-pennnet.upenn.edu>
2011-03-17 17:32:49 -0400

    added schema info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,33 @@
+# == Schema Information
+# Schema version: 20110307224716
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  username   :string(255)
+#  firstname  :string(255)
+#  lastname   :string(255)
+#  email      :string(255)
+#  privacy    :integer
+#  webpage    :string(255)
+#  time_zone  :integer
+#  bio        :text
+#  location   :string(255)
+#  language   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
   has_many :tweets
   belongs_to :user
   
+  attr_accessible :username, :firstname, :lastname, :email, :privacy, :webpage
+  attr_accessible :time_zone, :bio, :location, :language
   validates_presence_of :username, :firstname, :lastname, :email, :privacy
-  validates_format_of :email,
-                      :with =>  /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
-                      :message => "Invalid email format"
+  validates :username, :uniqueness => true
+  validates :email, :presence => true,
+                    :format =>  /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                    :uniqueness => {:case_sensitive => false}
+  
 end
