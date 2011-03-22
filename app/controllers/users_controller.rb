b14ed8c30b238ec2340f5a95e87a2a45d296766a
Zachary Wasserman <zachwass2000@gmail.com>
2011-03-22 14:09:49 -0400

    modified users_controller so that show can use id or username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,10 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.xml
   def show
-    @user = User.find_by_username(params[:username])
+    @user = User.find_by_username(params[:id])
+    if @user.nil?
+      @user = User.find(params[:id])
+    end
 
     respond_to do |format|
       format.html # show.html.erb
