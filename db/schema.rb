96e00e2a3441f789e07ea4e6f18f4adcd5eaaf2a
Zachary Wasserman <zachwass2000@gmail.com>
2011-04-19 02:15:59 -0400

    favorite tweets now working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418223441) do
+ActiveRecord::Schema.define(:version => 20110419060702) do
+
+  create_table "favoritetweets", :force => true do |t|
+    t.integer  "tweet_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "followslists", :force => true do |t|
     t.integer  "list_id"
